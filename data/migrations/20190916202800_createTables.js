efd434f0b7f6483468b8a0d691373486ee5c1648
Sandy <sandy.jernigan@gmail.com>
2019-09-19 00:50:40 -0400

    createTable successful

--- data/migrations/20190916202800_createTables.js
+++ data/migrations/20190916202800_createTables.js
@@ -1,135 +1,164 @@
 // npx knex migrate:make [migration-name]
 
 exports.up = function(knex) {
+  return knex.schema
+
+  // groups  
+  .createTable('groups', tbl => {
+    tbl.increments();
+    tbl.string('groupname').notNullable().unique();
+    tbl.text('description');
+  })
+
+  // locations
+  .createTable('locations', tbl => {
+    tbl.increments();
+    tbl.string('location').notNullable();
+    tbl.string('description');
+  })
+
+  // categories
+  .createTable('categories', tbl => {
+    tbl.increments();
+    tbl.string('category').notNullable();
+    tbl.string('description');
+  })
 
   // users
-  return knex.schema.createTable('users', users => {
-    users.increments();
-    users.string('username').notNullable().unique();
-    users.string('password').notNullable();
-    users.string('firstname').notNullable();
-    users.string('lastname').notNullable();
-    users.string('preferredname');
-    users.string('email').notNullable().unique();
+  .createTable('users', tbl => {
+    tbl.increments();
+    tbl.string('username').notNullable().unique();
+    tbl.string('password').notNullable();
+    tbl.string('firstname').notNullable();
+    tbl.string('lastname').notNullable();
+    tbl.string('preferredname');
+    tbl.string('email').notNullable().unique();
     tbl.integer('group_id').unsigned().notNullable()
       .references('id').inTable('groups')
       .onDelete('CASCADE').onUpdate('CASCADE');
-  });
+  })
 
-  // groups
-  return knex.schema.createTable('groups', tbl => {
+  // events
+  .createTable('events', tbl => {
     tbl.increments();
-    tbl.string('groupname').notNullable().unique();
-    users.text('description');
-  });
+    tbl.string('eventname').notNullable();
+    tbl.string('description');
+    tbl.date('eventdate');
+    tbl.time('eventtime');
+    tbl.integer('location_id').unsigned()
+      .references('id').inTable('locations')
+      .onDelete('CASCADE').onUpdate('CASCADE');
+    tbl.integer('user_id').unsigned().notNullable()
+      .references('id').inTable('users')
+      .onDelete('CASCADE').onUpdate('CASCADE');
+  })
+
+  // foods
+  .createTable('foods', tbl => {
+    tbl.increments();
+    tbl.string('foodname').notNullable();
+    tbl.string('description');
+    tbl.integer('category_id').unsigned().notNullable()
+      .references('id').inTable('categories')
+      .onDelete('CASCADE').onUpdate('CASCADE');
+    tbl.string('vegetarian');
+    tbl.string('vegan');
+    tbl.string('gutenfree');
+  })
+
+  // guests
+  .createTable('guests', tbl => {
+    tbl.increments();
+    tbl.string('guestname').notNullable();
+    tbl.string('guestemail').notNullable();
+    tbl.integer('user_id').unsigned()
+      .references('id').inTable('users');
+  })
+
+// Table relationships
 
   // user_group
-    return knex.schema.createTable('user_group', tbl => {
-      tbl.integer('user_id').unsigned().notNullable()
-        .references('id').inTable('users')
-        .onDelete('CASCADE').onUpdate('CASCADE');
-      tbl.integer('group_id').unsigned().notNullable()
-        .references('id').inTable('groups')
-        .onDelete('CASCADE').onUpdate('CASCADE');
-    });
+  .createTable('user_group', tbl => {
+    tbl.integer('user_id').unsigned().notNullable()
+      .references('id').inTable('users')
+      .onDelete('CASCADE').onUpdate('CASCADE');
+    tbl.integer('group_id').unsigned().notNullable()
+      .references('id').inTable('groups')
+      .onDelete('CASCADE').onUpdate('CASCADE');
+    tbl.primary(['user_id', 'group_id']);
+  })
 
-// ## events Table
+  // event_location
+  .createTable('event_location', tbl => {
+    tbl.integer('event_id').unsigned().notNullable()
+      .references('id').inTable('events')
+      .onDelete('CASCADE').onUpdate('CASCADE');
+    tbl.integer('location_id').unsigned().notNullable()
+      .references('id').inTable('locations');
+    tbl.primary(['event_id', 'location_id']);
+  })
 
-// Potluck events - will need events table for the individual events
+  // user_event
+  .createTable('user_event', tbl => {
+    tbl.integer('user_id').unsigned().notNullable()
+      .references('id').inTable('users')
+      .onDelete('CASCADE').onUpdate('CASCADE');
+    tbl.integer('event_id').unsigned().notNullable()
+      .references('id').inTable('events')
+      .onDelete('CASCADE').onUpdate('CASCADE');
+    tbl.primary(['user_id', 'event_id']);
+  })
 
-// | id | increments |
-// | eventname | string, notNullable |
-// | description | string |
-// | eventdate | date |
-// | eventtime | time |
-// | location_id | link to eventlocation table |
-// | user_id | links to the user_id who created the event |
+  // guests_events
+  .createTable('guests_events', tbl => {
+    tbl.integer('event_id').unsigned().notNullable()
+      .references('id').inTable('events')
+      .onDelete('CASCADE').onUpdate('CASCADE');
+    tbl.integer('guest_id').unsigned().notNullable()
+      .references('id').inTable('guests');
+    tbl.primary(['event_id', 'guest_id']);
+    tbl.string('attending');
+  })
 
-// ### user_event Table
+  // foods_category
+  .createTable('foods_category', tbl => {
+    tbl.integer('food_id').unsigned().notNullable()
+      .references('id').inTable('foods')
+      .onDelete('CASCADE').onUpdate('CASCADE');
+    tbl.integer('category_id').unsigned().notNullable()
+      .references('id').inTable('categories');
+    tbl.primary(['food_id', 'category_id']);
+  })
 
-// table to connect users and the event they created
-
-// | user_id        | integer, .unsigned().notNullable().references('id').inTable('users') |
-// | event_id       | integer, .unsigned().notNullable().references('id').inTable('events') |
-// tbl.primary(['user_id', 'event_id']);
-
-// ### locations Table
-
-// Locations that events can occur
-
-// | id | increments |
-// | location | string, notNullable |
-// | description | string |
-
-// ### event_location Table
-
-// Locations may be reused - multiple events can have the same location
-
-// | event_id        | integer, .unsigned().notNullable().references('id').inTable('events') |
-// | location_id       | integer, .unsigned().notNullable().references('id').inTable('locations') |
-// tbl.primary(['event_id', 'location_id']);
-
-
-// ## foods Table
-
-// Potluck Food - will need a table for different foods, what category the food belongs to, is it vegetarian/vegan friendly, and is it guten free
-
-// | id | increments |
-// | foodname | string, notNullable |
-// | description | string |
-// | category_id | links to category table |
-// | vegetarian | boolean |
-// | vegan | boolean | 
-// | gutenfree | boolean |
-
-// ## categories Table
-
-// Potluck Food categories
-
-// | id | increments |
-// | category | string, notNullable |
-// | description | string |
-
-// ### foods_category Table
-
-// multiple foods can be in different categories
-
-// | food_id        | integer, .unsigned().notNullable().references('id').inTable('foods') |
-// | category_id       | integer, .unsigned().notNullable().references('id').inTable('categories') |
-// tbl.primary(['food_id', 'category_id']);
-
-// ## guests Table
-
-// Guests may register and gain a username, however they do not have to register. Email required.
-
-// | id | increments |
-// | guestname | string, notNullable |
-// | guestemail | string, notNullable |
-// | user_id | link to user id optional |
-
-// ### guests_events
-
-// Guests may attend multiple different events and if attending: "going", "not going", or "unknown" may be null
-
-// | event_id    | integer, .unsigned().notNullable().references('id').inTable('events') |
-// | guest_id    | integer, .unsigned().notNullable().references('id').inTable('guests') |
-// | attending   | string | 
-// tbl.primary(['event_id', 'guest_id']);
-
-// ### guests_events
-
-// multiple foods can be in different events, and Guests should be able to select items to bring to an event
-
-// | food_id   | integer, .unsigned().notNullable().references('id').inTable('foods') |
-// | event_id  | integer, .unsigned().notNullable().references('id').inTable('events') |
-// | guest_id  | integer, .unsigned().notNullable().references('id').inTable('guests') |
-// | quantity  | integer, notNullable |
-// tbl.primary(['guest_id', 'food_id', 'event_id']);
-
+  // food_events
+  .createTable('food_events', tbl => {
+    tbl.integer('event_id').unsigned().notNullable()
+      .references('id').inTable('events')
+      .onDelete('CASCADE').onUpdate('CASCADE');
+    tbl.integer('food_id').unsigned().notNullable()
+      .references('id').inTable('foods');
+    tbl.integer('guest_id').unsigned().notNullable()
+      .references('id').inTable('guests');
+    tbl.primary(['event_id', 'food_id']);
+    tbl.integer('quantity').notNullable();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('users');
+  return knex.schema
+    .dropTableIfExists('food_events')
+    .dropTableIfExists('foods_category')
+    .dropTableIfExists('guests_events')
+    .dropTableIfExists('user_event')
+    .dropTableIfExists('event_location')
+    .dropTableIfExists('user_group')
+    .dropTableIfExists('guests')
+    .dropTableIfExists('events')
+    .dropTableIfExists('foods')
+    .dropTableIfExists('users')
+    .dropTableIfExists('categories')
+    .dropTableIfExists('locations')
+    .dropTableIfExists('groups');
 };
 
 // npx knex migrate:latest
