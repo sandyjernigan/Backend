fe1de389a992b518f10ede5bc7f64c990bd63918
Sandy <sandy.jernigan@gmail.com>
2019-09-29 21:22:28 -0400

    Index file requires server.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const server = require('./api/server.js');
 
 const PORT = process.env.PORT || 4000;
+
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
 });
